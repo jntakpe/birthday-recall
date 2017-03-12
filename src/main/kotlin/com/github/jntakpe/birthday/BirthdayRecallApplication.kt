758ade1e0f551d99a4cc394b8b681345b40c2a3d
jntakpe <j.ntakpe@gmail.com>
2017-03-12 21:51:14 +0100

    Generate projet with Spring Initializr

---
+++ src/main/kotlin/com/github/jntakpe/birthday/BirthdayRecallApplication.kt
@@ -0,0 +1,11 @@
+package com.github.jntakpe.birthday
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class BirthdayRecallApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(BirthdayRecallApplication::class.java, *args)
+}
